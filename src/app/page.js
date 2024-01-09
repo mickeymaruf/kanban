e17b70d35db9bc2037cd4ff33e1fbd92415e5d01
Md Maruf Hossain <mickeymaruf@gmail.com>
2024-01-09 14:52:42 +0600

    Sidebar deisgn created

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+
 export default function Home() {
   return (
     <div className="flex">
@@ -8,6 +10,14 @@ export default function Home() {
           <div className="h-6 w-1 bg-indigo-800/70 rounded-lg"></div>
           <h4 className="text-2xl font-semibold ml-2">kanban</h4>
         </div>
+
+        <ul className="pr-10">
+          <li className="pl-10 py-3 text-xs tracking-wider">ALL BOARDS (8)</li>
+          <li className="pl-10 py-3 hover:bg-indigo-600 text-gray-300 hover:text-white rounded-r-full text-sm flex gap-x-4 items-center duration-100"><MdOutlineSpaceDashboard size={20} /> Platform Launch</li>
+          <li className="pl-10 py-3 hover:bg-indigo-600 text-gray-300 hover:text-white rounded-r-full text-sm flex gap-x-4 items-center duration-100"><MdOutlineSpaceDashboard size={20} /> Marketing Plan</li>
+          <li className="pl-10 py-3 hover:bg-indigo-600 text-gray-300 hover:text-white rounded-r-full text-sm flex gap-x-4 items-center duration-100"><MdOutlineSpaceDashboard size={20} /> Roadmap</li>
+          <li className="pl-10 py-3 text-indigo-500 text-sm flex gap-x-4 items-center duration-100"><MdOutlineSpaceDashboard size={20} /> + Create New Board</li>
+        </ul>
       </aside>
       <main className="flex-1 bg-gray-900">
         <div className="bg-gray-800 py-8 px-5 border-b border-l border-gray-700">
